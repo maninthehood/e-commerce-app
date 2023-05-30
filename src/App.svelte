ea4b200baf02f8a2f6aef20232cccfbfcf0ead06
maninthehood <115598773+maninthehood@users.noreply.github.com>
2023-05-30 09:40:10 -0400

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Footer from './lib/Footer.svelte'
+  import Navbar from './lib/Navbar.svelte'
+  import Page from './lib/Page.svelte'
+</script>
+
+<main>
+  <Navbar/>
+  <Page/>
+  <Footer/>
+</main>
---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,50 @@
+<script>
+    
+</script>
+<main>
+<nav>
+    <ul>
+        <li>E-Commerce App</li>
+        <li> <span>0</span><img src="https://img.icons8.com/?size=1x&id=13014&format=png" alt="cart image"></li>
+    </ul>
+</nav>
+</main>
+
+<style>
+
+    *{
+        margin:0;
+        padding:0;
+        
+    }
+    span{
+        position: absolute;
+        top:0;
+        border-radius:999999px;
+    }
+    nav{
+        color:white;
+        padding:5px;
+        height:80px;
+        box-shadow:1px -5px 26px 10px blue;
+    }
+    ul{
+        display: flex;
+    }
+    li:nth-child(1){
+        font-size:30px;
+        margin-right:auto;
+        padding-top:20px;
+        padding-left:20px;
+    }
+    li:nth-child(2){
+        padding-top:13px;
+    }
+    ul>li{
+        list-style-type: none;
+    }
+    img{
+        width:50px;
+        height:50px;
+    }
+</style>
